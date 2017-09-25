fe7359518f820209021b01d3640ca1bd5dab88d3
Yaroslav Gaponov <yaroslav.gaponov@avid.com>
2017-09-25 08:03:34 -0400

    fix

--- tinyfly.js
+++ tinyfly.js
@@ -122,6 +122,9 @@ class Index {
         }
         return hash >>> 0;
     }
+    static getNodeBlockOffset(index) {
+        return index + (index<<1);
+    }
     clear() {
         this._bitset.clear();
         for (let i = 0; i < this._table.length; i++) {
@@ -129,112 +132,90 @@ class Index {
         }
         return this;
     }
-    has(key) {
+    get(key, check) {
         const hash = Index.calc_hash(key);
         const index = hash % this._table.length;
-        let offset = this._table[index];
+
+        let curr_offset = this._table[index];
         for (;;) {
-            if (offset === EOC) {
-                return false;
+            if (curr_offset === EOC) {
+                return -1;
             }
-            const curr_hash = this._nodes[offset];
-            const curr_next = this._nodes[offset + 2];
-            if (hash === curr_hash) {
-                return true;
+            let _addr = Index.getNodeBlockOffset(curr_offset);
+            const curr_hash = this._nodes[_addr];
+            const curr_id = this._nodes[_addr + 1];
+            if (hash === curr_hash && check(curr_id)) {
+                return curr_id;
             } else if (hash > curr_hash) {
-                return false;
+                return -1;
             } else {
-                offset = curr_next;
+                curr_offset =  this._nodes[_addr + 2];
             }
-        }
+        }        
     }
-    set(id, key) {
+    has(key, check) {
+        return this.get(key, check) !== -1;
+    }
+    set(id, key, check) {
         const hash = Index.calc_hash(key);
         const index = hash % this._table.length;
+
         let pred_offset = EOC;
-        let offset = this._table[index];
+        let curr_offset = this._table[index];
         for (;;) {
-            if (offset === EOC) {
-                let idx = this._bitset.fetch();
-                idx = idx + idx << 1;
-                this._nodes[idx] = hash;
-                this._nodes[idx + 1] = id;
-                this._nodes[idx + 2] = offset;
+            let _addr = Index.getNodeBlockOffset(curr_offset);
+            const curr_hash = this._nodes[_addr];
+            const curr_id = this._nodes[_addr + 1];
+            const curr_next = this._nodes[_addr + 2];
+            if (hash === curr_hash && check(curr_id)) {
+                return false;            
+            } else if (curr_offset === EOC || hash > curr_hash) {
+                const new_offset = this._bitset.fetch();
+                let _addr = Index.getNodeBlockOffset(new_offset); 
+                this._nodes[_addr] = hash;
+                this._nodes[_addr + 1] = id;
+                this._nodes[_addr + 2] = EOC;
                 if (pred_offset === EOC) {
-                    this._table[index] = idx;
+                    this._table[index] = new_offset;
                 } else {
-                    this._nodes[pred_offset + 2] = idx;
+                    let _addr = Index.getNodeBlockOffset(pred_offset);
+                    this._nodes[_addr + 2] = new_offset;
                 }
                 return true;
-            }
-            const curr_hash = this._nodes[offset];
-            const curr_next = this._nodes[offset + 2];
-            if (hash === curr_hash) {
-                this._nodes[offset + 1] = id;
-                return true;
-            } else if (hash > curr_hash) {
-                let idx = this._bitset.fetch();
-                idx = idx + idx << 1;
-                this._nodes[idx] = hash;
-                this._nodes[idx + 1] = id;
-                this._nodes[idx + 2] = offset;
-                if (pred_offset === EOC) {
-                    this._table[index] = idx;
-                } else {
-                    this._nodes[pred_offset + 2] = idx;
-                }
-                return true;
-            } else if (hash < curr_hash) {
-                pred_offset = offset;
-                offset = curr_next;
+            } else {
+                pred_offset = curr_offset;
+                curr_offset = curr_next;
             }
         }
 
     }
-    get(key) {
+    delete(key, check) {        
         const hash = Index.calc_hash(key);
         const index = hash % this._table.length;
-        let offset = this._table[index];
-        for (;;) {
-            if (offset === EOC) {
-                return -1;
-            }
-            const curr_hash = this._nodes[offset];
-            const curr_id = this._nodes[offset + 1];
-            const curr_next = this._nodes[offset + 2];
-            if (hash === curr_hash) {
-                return curr_id;
-            } else if (hash > curr_hash) {
-                return -1;
-            } else {
-                offset = curr_next;
-            }
-        }
-    }
-    delete(key) {
-        const hash = Index.calc_hash(key);
-        const index = hash % this._table.length;
+    
         let pred_offset = EOC;
-        let offset = this._table[index];
-        for (;;) {
-            if (offset === EOC) {
+        let curr_offset = this._table[index];
+        for (;;) {            
+            if (curr_offset === EOC) {
                 return -1;
             }
-            const curr_hash = this._nodes[offset];
-            const curr_id = this._nodes[offset + 1];
-            const curr_next = this._nodes[offset + 2];
-            if (hash === curr_hash) {
+            let _addr = Index.getNodeBlockOffset(curr_offset);
+            const curr_hash = this._nodes[_addr];
+            const curr_id = this._nodes[_addr + 1];
+            const curr_next = this._nodes[_addr + 2];
+            if (hash === curr_hash && check(curr_id)) {
                 if (pred_offset === EOC) {
                     this._table[index] = curr_next;
                 } else {
-                    this._nodes[pred_offset + 2] = curr_next;
+                    let _addr = Index.geNodeBlockOffset(pred_offset);
+                    this._nodes[_addr + 2] = curr_next;
                 }
                 return curr_id;
             } else if (hash > curr_hash) {
                 return -1;
             } else {
-                pred_offset = offset;
-                offset = curr_next;
+                pred_offset = curr_offset;
+                curr_offset = curr_next;
             }
         }
     }
@@ -246,24 +227,40 @@ class NoSql {
         this._storage = storage;
     }
     has(key) {
-        return this._index.has(key);
+        return this._index.has(key,
+            (id) => {
+                return this._storage.getKey(id) === key;
+            }
+        );
     }
     set(key, value) {
         const id = this._storage.save(key, value);
         if (id === -1) {
             return false;
         }
-        return this._index.set(id, key);
+        return this._index.set(id, key,
+            (id) => {
+                return this._storage.getKey(id) === key;
+            }
+        );
     }
     get(key) {
-        const id = this._index.get(key);
+        const id = this._index.get(key,
+            (id) => {
+                return this._storage.getKey(id) === key;
+            }
+        );
         if (id === -1) {
             return;
         }
         return this._storage.getValue(id);
     }
     delete(key) {
-        const id = this._index.delete(key);
+        const id = this._index.delete(key,
+            (id) => {
+                return this._storage.getKey(id) === key;
+            }
+        );
         if (id === -1) {
             return false;
         }
@@ -290,8 +287,8 @@ net.createServer(
         };
         socket.on('data', (chunk) => {
             const [header, body] = chunk.toString().split(LN + LN);
-            const [method, path] = header.split(LN)[0].split(' ');
-            const key = path.slice(1);
+            const [method, path] = header.split(LN)[0].split(' ');            
+            const key = path.startsWith('/') ? path.slice(1) : path;
             switch(method) {
                 case 'HEAD': 
                     return reply(nosql.has(key) ? 200 : 404);
@@ -306,7 +303,7 @@ net.createServer(
                     return reply(nosql.set(key, body) ? 200 : 500);
 
                 case 'DELETE': 
-                    return reply(nosql.delete(key, body) ? 200 : 404);                
+                    return reply(nosql.delete(key) ? 200 : 404);                
             }
             return reply(501);
         });
